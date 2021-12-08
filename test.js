f024de168bca11f1e51b1d0de4faf7b50132eef3
borysl <borys.lebeda@gmail.com>
2021-12-08 19:55:12 +0200

    Add succesful test

---
+++ test.js
@@ -0,0 +1,32 @@
+console.log('Ти признайся мені,');
+console.log('Звідки в тебе ті чари,');
+console.log('Я без тебе всі дні');
+console.log('У полоні печалі.');
+console.log('Може, десь у лісах');
+console.log('Ти чар-зілля шукала,');
+console.log('Сонце-руту знайшла');
+console.log('І мене зчарувала?');
+
+console.log('Приспів:');
+
+console.log('Червону руту');
+console.log('Не шукай вечорами, —');
+console.log('Ти у ме_не єдина,');
+console.log('Тільки ти, повір');
+console.log('Бо твоя врода —');
+console.log('То є чистая вода,');
+console.log('То є би_страя вода');
+console.log('З синіх гір.');
+
+console.log('Бачу я тебе в снах,');
+console.log('У дібровах зелених');
+console.log('По забутих стежках');
+console.log('Ти приходиш до мене.');
+
+console.log('І не треба нести');
+console.log('Мені квітку надії,');
+console.log('Бо давно уже ти');
+console.log('Увійшла в мої мрії.');
+console.log('Приспів:');
+console.log('То є би_страя вода');
+console.log('З синіх гір.');
